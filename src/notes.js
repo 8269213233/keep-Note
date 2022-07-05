388d94ae2167d25b326b7d6ec1513e6b17a3c980
YOGESH VISHWAKARMA <61071221+8269213233@users.noreply.github.com>
2022-07-05 11:24:05 +0530

    first commit

---
+++ src/notes.js
@@ -0,0 +1,30 @@
+const notes=[
+    {
+        key:1,
+        title:"Delegation",
+        content:"Q. How many programmers does it takes to change a light bukb?"
+    },
+    {
+        key:2,
+        title:"Loops",
+        content:"Q. How to keep a programmer in a shower forever?"
+    },
+    {
+        key:3,
+        title:"Arrays",
+        content:"Q. Why did the programmer left his job?"
+    },
+    {
+        key:4,
+        title:"Hardware vs software",
+        content:"Q. What is the difference between hardware and software?"
+    },
+    {
+        key:5,
+        title:"Big Ideas",
+        content:"Eat more sushi?"
+    }
+
+]
+
+export default notes;
